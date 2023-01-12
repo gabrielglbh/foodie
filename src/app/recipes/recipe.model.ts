6f2f11e11ee1d9e9d94ebc5713cb72d0abe72429
Gabriel Garcia <devgglop@gmail.com>
2023-01-12 12:44:02 +0100

    Shopping List HTML and Ingredients

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,7 @@
 export class Recipe {
-    public name: String
-    public description: String
+  constructor(
+    public name: String,
+    public description: String,
     public imagePath: String
-
-    constructor(name: String, description: String, imagePath: String) {
-        this.name = name
-        this.description = description
-        this.imagePath = imagePath
-    }
+  ) {}
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Eggs', 5),
+    new Ingredient('Bacon', 1),
+  ];
 }
