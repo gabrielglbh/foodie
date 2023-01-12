7e944cf107ea941428dcd401adad8b892dff4ec9
Gabriel Garcia <devgglop@gmail.com>
2023-01-12 12:18:08 +0100

    Added Recipe Model and display withou recipe-item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Bacon and eggs',
+      'Bacon and eggs',
+      'https://i.dietdoctor.com/wp-content/uploads/2015/12/DD-14.jpg?auto=compress%2Cformat&w=1600&h=1067&fit=crop'
+    ),
+    new Recipe(
+      'Bacon and eggs',
+      'Bacon and eggs',
+      'https://i.dietdoctor.com/wp-content/uploads/2015/12/DD-14.jpg?auto=compress%2Cformat&w=1600&h=1067&fit=crop'
+    ),
+    new Recipe(
+      'Bacon and eggs',
+      'Bacon and eggs',
+      'https://i.dietdoctor.com/wp-content/uploads/2015/12/DD-14.jpg?auto=compress%2Cformat&w=1600&h=1067&fit=crop'
+    ),
+  ];
 }
